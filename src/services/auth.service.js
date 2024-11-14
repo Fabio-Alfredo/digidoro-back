e17b84f7c1684ef41f67bfc2801f078f5fda4937
Fabio-Alfredo <00210121@uca.edu.sv>
2024-11-13 23:50:43 -0600

    add: function login user created

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,25 +1,38 @@
-import { authErrorCodes } from "../utils/errors/auth.errorCodes";
-import * as userReposiry from '../repositories/user.repository';
-import { ServiceError } from "../errors/servise.error";
+import { authErrorCodes } from "../utils/errors/auth.errorCodes.js";
+import * as userReposiry from "../repositories/user.repository.js";
+import { ServiceError } from "../errors/servise.error.js";
+import { createToken } from "../utils/jwt.util.js";
 
-export const register = async(user)=>{
-    try{
-        const existUser = await userReposiry.findUserByEmail(user.email);
-        if(existUser) throw new Error(authErrorCodes.USER_ALREADY_EXISTS);
+export const register = async (user) => {
+  try {
+    const existUser = await userReposiry.findUserByEmail(user.email);
+    if (existUser) throw new Error(authErrorCodes.USER_ALREADY_EXISTS);
 
-        const newUser = await userReposiry.createUser(user);
-        return newUser;
-    }catch(e){
-        throw new ServiceError('Register error', e.code || authErrorCodes.FAILD_TO_CREATE_USER);
-    }
-}
+    const newUser = await userReposiry.createUser(user);
+    return newUser;
+  } catch (e) {
+    throw new ServiceError(
+      "Register error",
+      e.code || authErrorCodes.FAILD_TO_CREATE_USER
+    );
+  }
+};
 
-export const login = async(email, password)=>{
-    try{
-        const existUser = await userReposiry.findUserByEmail(email);
-        if(!existUser || !(await existUser.comparePassword(password)))
-            throw new Error(authErrorCodes.INVALID_CREDENTIALS);
-    }catch(e){
-        throw new ServiceError('Login error', e.code || authErrorCodes.INVALID_CREDENTIALS);
-    }
-}
+export const login = async (email, password) => {
+  try {
+    const existUser = await userReposiry.findUserByEmail(email);
+
+    if (!existUser || !(await existUser.comparePassword(password)))
+      throw new Error(authErrorCodes.INVALID_CREDENTIALS);
+
+    const token = createToken({ id: existUser._id });
+    if (!token) throw new Error(authErrorCodes.FAILD_CREATE_TOKEN);
+
+    return token;
+  } catch (e) {
+    throw new ServiceError(
+      "Login error",
+      e.code || authErrorCodes.INVALID_CREDENTIALS
+    );
+  }
+};
---
+++ src/utils/jwt.util.js
@@ -0,0 +1,12 @@
+import { sign, verify  } from "jsonwebtoken";
+import { config } from "../configs/config.js";
+
+export const createToken = (payload) => {
+  const token = sign(payload, config.jwSecret, { expiresIn: "1d" });
+  return token;
+};
+
+export const verify = (token) => {
+  const payload = verify(token, config.jwSecret);
+  return payload;
+};
