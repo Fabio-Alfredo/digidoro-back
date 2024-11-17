365c0bc2f99e7e9b71aa41a549a3ff86fab371d6
Fabio-Alfredo <00210121@uca.edu.sv>
2024-11-16 22:48:53 -0600

    update: register user and update states pomodoro

--- src/models/pomodoro.model.js
+++ src/models/pomodoro.model.js
@@ -1,24 +1,29 @@
 import { Schema, model } from "mongoose";
+import { STATE } from "../utils/pomodoro.utils";
 
 const pomodoroSchema = new Schema(
   {
     time: {
-      type: Number, // seconds
+      type: Number,
       required: true,
+      default: 1500,
     },
     state: {
       type: String,
-      enum: ["break", "work", "pause"],
+      enum: [STATE.WORK, STATE.BREAK, STATE.NONE],
       required: true,
+      default: STATE.NONE,
     },
     id_user: {
       type: Schema.Types.ObjectId,
       ref: "User",
+      required: true,
     },
     task_list: [
       {
         type: Schema.Types.ObjectId,
         ref: "Todo",
+        default: [],
       },
     ],
   },
