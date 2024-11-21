7fa6f58259b96af11b89480b16c0a3a0d8133199
Fabio Hernandez <101723049+Fabio-Alfredo@users.noreply.github.com>
2024-11-21 17:03:20 -0600

    Update app.js

--- app.js
+++ app.js
@@ -3,9 +3,10 @@ import { config } from './src/configs/config.js';
 import { dbConnection } from './src/configs/dbConnection.config.js';
 import  routes  from './src/routes/index.route.js';
 import { errorHandler } from './src/middlewares/errorHanddler.middleware.js';
+import cors from 'cors'
 
 const app =express();
-
+app.user(cors());
 const PORT = config.port|| 3002;
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
